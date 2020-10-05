f8c4d33092bc180477c8b584ffe6d292527df8b5
dongdong-hue <1365730057@qq.com>
2020-10-05 16:13:25 +0800

    add

---
+++ mmy/main.cpp
@@ -0,0 +1,39 @@
+﻿#include <iostream>
+
+using namespace std;
+char key[256]=" ";
+int main()
+{
+    if(key==" ")
+    cout<<key<<endl;
+    else
+    cout<<"key isn't NULL"<<endl;
+    return 0;
+}
+
+/*
+int main()
+{
+    char c;
+    cout<<"Enter a character:";
+    cin>>c;
+    cout<<"It is a:";
+    switch (c) {
+        case '0':
+        case '1':
+        case '2':
+        case '3':
+        case '4':
+        case '5':
+        case '6':
+        case '7':
+        case '8':
+        case '9':
+        cout<<"digiter."<<endl;
+        break;
+    default:
+        cout<<"non-digiter."<<endl;
+        break;
+    }
+    return 0;
+}*/
